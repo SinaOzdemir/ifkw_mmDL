9aa4cc7b564e1b515880f1bcc4e4cbadd16e6e90
SinaOzdemir <sina.ozdemir@ntnu.no>
2022-05-10 11:42:39 +0200

    working image scraper

--- Scripts/02_image_scraper.R
+++ Scripts/02_image_scraper.R
@@ -21,7 +21,8 @@ data_source<- here("Data","labelled_data")
 
 data_dest <- here("Data","image_data")
 
-dt<- readRDS(file = file.path(data_source,"DL_data.RDS"))
+dt<- readRDS(file = file.path(data_source,"DL_data.RDS")) %>% 
+  filter(!duplicated(status_id))
 
 
 
@@ -33,12 +34,17 @@ test_data<- dt %>%
   select(status_id,media_url) %>% 
   slice_sample(n =5)
 
-
-#doesn't seem to pick up the image... it could be because of the link
-ralger::images_scrap(link = "http://pbs.twimg.com/media/EJKrHGBWoAA1yd0.jpg",imgpath = file.path(data_dest,"test_image"),extn = "jpeg")
+download.file(url = test_data[1,2],destfile = paste0(data_dest,"/test_image.jpeg"),mode = "wb")
 
 
 
-a<-images_scrap(link = "https://rstudio.com/",
-             imgpath = here("Data"),
-             extn = "png")
+# full scraping -----------------------------------------------------------
+
+for (i in 1:nrow(dt)) {
+  img_url<- dt[i,"media_url"]
+  img_ext<- tail(unlist(str_split(string = dt[i,"media_url"],pattern = "\\.",simplify = F)),n=1)
+  img_name<- paste0(dt[i,"screen_name"],"-",dt[i,"status_id"],".",img_ext)
+  img_path<- paste(data_dest,img_name,sep = "/")
+  download.file(url = img_url,destfile = img_path,mode = "wb")
+ 
+}
